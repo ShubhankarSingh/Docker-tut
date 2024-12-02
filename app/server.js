d881b278c5c6173e9b8e1e2730404d9b9a91fe34
Shubhankar Singh <singhshubhankar01@gmail.com>
2024-12-02 22:40:07 +0530

    first commit

---
+++ app/server.js
@@ -0,0 +1,27 @@
+let express = require('express');
+let path = require('path');
+let fs = require('fs');
+let app = express();
+
+
+app.get('/', function (req, res) {
+    
+});
+
+app.get('/profile-picture', function (req, res) {
+  
+});
+
+
+
+app.post('/update-profile', function (req, res) {
+  
+});
+
+app.get('/get-profile', function (req, res) {
+  
+});
+
+app.listen(3000, function () {
+  console.log("app listening on port 3000!");
+});
